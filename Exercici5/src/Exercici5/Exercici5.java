a1f561ca8c2c474e172f027e9dd51ea3012b0896
Josep <jfalagan20987@iesjoanramis.org>
2024-10-10 20:07:47 +0200

    commit mods

--- Exercici5/src/Exercici5/Exercici5.java
+++ Exercici5/src/Exercici5/Exercici5.java
@@ -5,6 +5,10 @@ public class Exercici5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Això és l'exercici 5");
+		
+		System.out.println("Modificació a master");
+		
 		System.out.println("Modificació mods");
 	}
 
